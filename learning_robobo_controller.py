6e5d4656cf54292b0e787a1f6e56eab348858aae
YingjieGe1 <147314086+YingjieGe1@users.noreply.github.com>
2024-06-07 23:02:33 +0200

    Update learning_robobo_controller.py

--- learning_robobo_controller.py
+++ learning_robobo_controller.py
@@ -22,10 +22,9 @@ def move_and_avoid_obstacles(rob) -> None:
         list_sensors = [irs[7], irs[2], irs[4], irs[3], irs[5]]
         lists_sensors.append(list_sensors)
 
-        if irs[2] is not None and irs[2] > front_ir_threshold:  # Check FrontL sensor
-            # Obstacle detected, turn right
+        if irs[2] is not None and irs[2] > front_ir_threshold:  
             print("turn right")
-            rob.move_blocking(50, -50, 620)  # Turn right for 500 ms
+            rob.move_blocking(50, -50, 620) 
             rob.move_blocking(20, 20, 3000)
             break
 
@@ -37,7 +36,6 @@ def move_and_avoid_obstacles(rob) -> None:
         start_time = time.time()
         current_time = time.time()
 
-        # 检查是否超过了设定的持续时间
         if current_time - start_time > duration:
             print("Time's up! Exiting the loop.")
             break
@@ -61,12 +59,11 @@ def move_and_avoid_obstacles_back(rob) -> None:
         print(irs[7], irs[2], irs[4], irs[3], irs[5])
 
         if irs[4] is not None and irs[4] > front_ir_threshold:
-            # 检测到障碍物，后退
-            rob.move_blocking(-50, -50, 99000)  # 后退 500 毫秒
+            
+            rob.move_blocking(-50, -50, 99000) 
             break
         else:
-            # 无障碍物，向前移动
-            rob.move_blocking(40, 40, 100)  # 向前移动 100 毫秒
+            rob.move_blocking(40, 40, 100)  
 
 
     if isinstance(rob, SimulationRobobo):
